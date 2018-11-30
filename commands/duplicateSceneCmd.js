d20612917bf26ed4bf373ac4cde3416ecf16b364
migueluji <migueluji@gmail.com>
2018-11-30 15:51:37 +0100

    Arreglado bug en comando duplicar escena

--- commands/duplicateSceneCmd.js
+++ commands/duplicateSceneCmd.js
@@ -2,7 +2,7 @@ class DuplicateSceneCmd extends Command {
 
     constructor (sceneID){
         super();
-        this._pos=this.editor.model.list.findIndex(i => i.id == sceneID);
+        this._pos=this._editor.model.list.findIndex(i => i.id == sceneID);
         this._scene=new Scene(); //asi obtiene un nuevo identificador
         this._scene.name=this._editor.model.list[this._pos].name+" (copy)";//solo tiene en cuenta el nombre
         this._type="DuplicateSceneCmd";
