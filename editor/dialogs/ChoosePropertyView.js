a5d9551c7a1ee5bafd4c730b204ed3a9595341cd
migueluji <migueluji@gmail.com>
2020-10-06 16:55:54 +0200

    iris bug fixed

--- editor/dialogs/ChoosePropertyView.js
+++ editor/dialogs/ChoosePropertyView.js
@@ -45,6 +45,7 @@ class ChoosePropertyView {
 	addFields(parameters){
 		for (const field in parameters) {
 			var list=null;
+			var type;
 			switch(field){
 				case "Element" : type="select"; list=["Game","Me"]; list=list.concat(Command.getActorListCmd()); break;
 				case "Property" : type="select"; list=Command.getPropertiesListCmd(parameters.Element,this.option); list.unshift("");break;
